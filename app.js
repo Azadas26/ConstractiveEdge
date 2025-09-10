0286e9004bb4aa2a0ad0be4bb8738e3487873ecf
Azadas26 <azad45518@gmail.com>
2025-09-11 00:53:10 +0530

    Admin Auth Authentication Addedd

--- app.js
+++ app.js
@@ -7,11 +7,15 @@ var hbs = require('express-handlebars')
 var db = require('./connection/connect')
 var fileUpload = require('express-fileupload')
 var session = require('express-session')
+var db = require('./connection/connect')
+const consts = require('./connection/constants')
+
+
 
 var userRouter = require('./routes/users');
 var adminRouter = require('./routes/admin');
 var workerRouter = require('./routes/workers');
-var constructiveedge= require('./routes/starting');
+var constructiveedge = require('./routes/starting');
 
 var app = express();
 
@@ -26,29 +30,31 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(fileUpload())
-app.use(session({secret:"key",cookie:{maxAge:600000}}))
+app.use(session({ secret: "key", cookie: { maxAge: 600000 } }))
+app.use((req, res, next) => {
+  adminRegisterCheck()
+  next()
+})
 
 
 app.use('/', userRouter);
 app.use('/admin', adminRouter);
 app.use('/workers', workerRouter);
-app.use('/constructiveedge', constructiveedge);
+app.use('/worklink', constructiveedge);
 
-db.connection().then((data)=>
-{
+db.connection().then((data) => {
   console.log(data);
-}).catch((err)=>
-{
+}).catch((err) => {
   console.log(err);
 })
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
@@ -57,5 +63,30 @@ app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.render('error');
 });
+
+async function adminRegisterCheck() {
+  try {
+    const admin = await db.get().collection(consts.admin_base).find().toArray();
+
+    if (admin.length != 0) {
+      console.log("Admin Already Registered");
+      return;
+    }
+    const admin_credentials = {
+      name: "admin",
+      password: "admin",
+      displayname: "W-Link admin 1"
+    }
+
+    await db.get().collection(consts.admin_base).insertOne(admin_credentials);
+    console.log("Admin Auto Registration Success");
+
+    return;
+  } catch (error) {
+    console.log(error);
+    return;
+
+  }
+}
 
 module.exports = app;
